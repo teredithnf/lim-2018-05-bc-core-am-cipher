352671602fc25323fa0ed276322b16f25ae9bec4
Teresa Navarro <mardalu@interscope.local>
2018-05-24 00:42:38 -0500

    actualizando lo necesesario para npm test

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,14 @@
 window.cipher = {
   // ... alert('aqui esta mi funcion de decode y encode')
+
+  encode: function(){
+    return;
+  },
+  decode: function(){
+    return;
+  },
+  createCipherWithOffset: function(){
+    return;
+  }
+
 };
