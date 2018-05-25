d6c2dec4386e424d1b9e3739c742d01ee9c56e92
Teresa Navarro <mardalu@interscope.local>
2018-05-25 07:06:14 -0500

    actualizando mi codigo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-alert('aqui voy a manipular el dom')
+
+let btnCifrar =  document.getElementById('btnCifrar');
+btnCifrar.addEventListener("click",() => {
+    let offset = document.getElementById("txtOffset").value;
+    let ingMensaje = document.getElementById("ingMensaje").value;
+    let encode = cipher.encode(offset, ingMensaje);
+    document.getElementById("desMensaje").value = encode;
+} );
+
+
+let btnLimpiar = document.getElementById("btnLimpiar");
+btnLimpiar.addEventListener("click",() => {
+    document.getElementById("txtOffset").value = "";
+    document.getElementById("ingMensaje").value = "";
+    document.getElementById("desMensaje").value = "";
+});
