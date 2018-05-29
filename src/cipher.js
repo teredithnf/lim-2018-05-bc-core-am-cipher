4afd8c43f16cf6f4c11eba100a262c83d818ed76
teredithnf <tere.navarro85@gmail.com>
2018-05-29 15:20:59 -0500

    cambios mayusculas y espacios

--- src/cipher.js
+++ src/cipher.js
@@ -3,18 +3,22 @@ window.cipher = {
   encode: (offset, string) => {
     let mensajeCodificado = '';
       for (let indice = 0; indice < string.length; indice++) {
-        let codigoCaracter = string.charCodeAt(indice); // de letra a numero
+        let codigoCaracter = string.toUpperCase().charCodeAt(indice); // de letra a numero
+        if (codigoCaracter === 32){
+          mensajeCodificado += " "
+        } else {
         let nuevoCaracter = (codigoCaracter - 65 + offset)%26 + 65;
         let nuevoCaracterCodificado = String.fromCharCode(nuevoCaracter); //de numero a letra
         //mensajeCodificado = mensajeCodificado + nuevoCaracterCodificado;
         mensajeCodificado += nuevoCaracterCodificado;
+        }
       }
     return mensajeCodificado;
   },
   decode:(offset, string) =>{
     let mensajeDescifrado = '';
     for (let indice = 0; indice < string.length; indice++){
-      let codLetra = string.charCodeAt(indice);
+      let codLetra = string.toUpperCase().charCodeAt(indice);
       let numeroAscii = (codLetra  + 65 - offset )%26 + 65 ;
       let letraAscii = String.fromCharCode(numeroAscii);
       mensajeDescifrado += letraAscii;
